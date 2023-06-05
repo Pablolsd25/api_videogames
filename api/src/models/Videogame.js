fcf03716f48965f0a6dbd7cb8f52a672953d83e7
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-06-05 11:41:09 -0600

    creacion de modelos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,37 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+
+
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+     
+    description:{
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+     
+    realeaseDate: {
+     type: DataTypes.DATE
+    },
+
+    rating:{
+        type: DataTypes.FLOAT,
+       allowNull: true,
+      },
+     
+    platforms:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   });
 };
