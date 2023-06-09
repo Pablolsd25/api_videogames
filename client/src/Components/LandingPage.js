a48cbe98b499899a2faa9d2cd10454ea2f74bf14
Pablolsd2501~ <pablot_comercial.alamo@hotmail.com>
2023-06-09 11:49:20 -0600

    Se crea NavBar falta terminar landing page

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import '../Components/LandingPage.css'
-
+import NavBar from './NavBar'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
-    <h1>Título de la Landing Page</h1>
-    <p>Descripción de la página</p>
+    <NavBar/>
+    <Card className="text-center">
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+    
    
   </div>
   )
